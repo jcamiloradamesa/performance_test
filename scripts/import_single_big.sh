8d96cd353fd6ca18d6d92a7451d9498e5566829d
Juan Camilo Rada Mesa <jcamilo.radamesa@aurea.com>
2018-10-03 14:02:03 -0500

    fixed

--- scripts/import_single_big.sh
+++ scripts/import_single_big.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' -d 'repositoryId=11000&dfScmUrl=https%3A%2F%2Fgithub.com%2Ftrilogy-group%2Fjive-cloud-application.git%3Fbranch%3Ddevelop&branch=develop&replayHistory=true&credentialsId=1' 'http://localhost:8080/api/v1.1/repositories'
+curl -X POST --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' -d 'repositoryId=11000&dfScmUrl=https%3A%2F%2Fgithub.com%2Ftrilogy-group%2Fjive-cloud-application.git%3Fbranch%3Ddevelop&branch=develop&replayHistory=true&credentialsId=1&initialBranch=development' 'http://localhost:8080/api/v1.1/repositories'
